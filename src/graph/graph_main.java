05f82d2abe6564b13a1fb5e23d314a59524a08db
iroziro <iroziro0526@gmail.com>
2017-01-23 17:00:14 +0900

    20170116 submit

---
+++ src/graph/graph_main.java
@@ -0,0 +1,16 @@
+package graph;
+
+public class graph_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		graph_view graph = new graph_view();
+		graph.setBounds(5,5,655,455);
+		graph.setVisible(true);
+		
+
+	}
+
+}
+
